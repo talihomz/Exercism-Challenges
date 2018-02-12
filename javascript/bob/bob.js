2be565473e04b0e654ecbbe582a73348e22b0d66
Kevin Wahome <wahome@talicraft.com>
2018-02-12 10:06:30 +0300

    Solved for 2 responses.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -4,6 +4,7 @@ function Bob() {
 
 Bob.prototype.hey = function(input) {
 
+
   // Whatever.
   // 1. Ends with a character
   // 2. Ends with whitespace
@@ -16,12 +17,17 @@ Bob.prototype.hey = function(input) {
 
   // Sure.
   // 1. Ends with a question mark
-  // 2. Have  whitespaces after the last question mark
+  // 2. Have whitespaces after the last question mark
+  if(input.match(/\?[\s]*$/))
+    return "Sure.";
+
 
   // Fine. Be that way!
   // 1. Whitespace only
   // 2. Empty starting
   // 3. Escape characters
+  if(input.length == 0 || input.match(/[\x00-\x1F\x7F]/))
+    return "Fine. Be that way!";
 
 }
 
