b440bf24d4cfc5c8f10d8ab8f21acc2bbb4e4ff3
Sava <vuckovicsava11@gmail.com>
2018-02-21 08:06:11 +0100

    solving raindrops..

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,19 @@
+class Raindrops
+
+  def self.convert(n)
+    output = ''
+    divisor = 1
+
+=begin
+    output = [2, 3, 7].each do |num|
+      until divisor == n
+        # CONTINUE
+        divisor += 1
+      end
+    end
+=end
+
+    output
+  end
+
+end
