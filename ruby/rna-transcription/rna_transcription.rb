07c39028ddc5b467a36d5194d5e11f8e0759238a
Sava <vuckovicsava11@gmail.com>
2018-02-21 07:47:54 +0100

    rna transcription solved

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,7 +1,21 @@
 class Complement
 
+  @@values = {
+    "C" => "G",
+    "G" => "C",
+    "T" => "A",
+    "A" => "U"
+  }
+
   def self.of_dna input
     return '' if input.match(/^[CGTA]+$/) == nil
+
+    output = ''
+    input.split('').each do |x|
+      output << @@values[x]
+    end
+
+    output
   end
 
 end
