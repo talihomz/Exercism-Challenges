a9722739165b6cc41792374e411cc3e79ab24c67
Kevin Wahome <wahome@talicraft.com>
2018-02-26 10:37:16 +0300

    Added first logic to converter

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,47 @@
+class Fixnum
+  @@roman_converter = [
+    {
+      range: "1 - 10",
+      lower: 'I',
+      mid: 'V'
+    },
+    {
+      range: "11 - 100",
+      lower: 'X',
+      mid: 'L'
+    },
+    {
+      range: "101 - 1000",
+      lower: 'C',
+      mid: 'D'
+    },
+    {
+      range: "1001 - n",
+      lower: 'M'
+    }
+  ]
+
+  def to_roman
+    result = []
+    self.to_s.split('').collect { |n| n.to_i }.reverse.each_with_index do |number, idx|
+
+      mid = @@roman_converter[idx][:mid]
+      lower = @@roman_converter[idx][:lower]
+      next_lower = idx == 3 ? '' : @@roman_converter[idx][:lower] * 10
+
+      if number < 4
+        result << number.times { result << "#{lower}" }
+      elsif number == 4
+        result << "#{lower}#{mid}"
+      elsif number == 5
+        result << "#{mid}"
+      elsif number < 9
+        result <<  "#{mid}" + (number - 5).times { "#{lower}" }
+      else
+        result << "#{next_lower}"
+      end
+    end
+
+    return result.join('')
+  end
+end
