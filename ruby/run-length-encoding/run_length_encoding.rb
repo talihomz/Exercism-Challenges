12bc41ca0ec897f6f52f585a43e17bbd7b2cca76
Kevin Wahome <wahomekevin@gmail.com>
2018-03-02 11:32:52 +0300

    Added RLE solution

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -2,7 +2,6 @@ class RunLengthEncoding
   def self.encode(input)
     similar_count = 1
     encoded = ""
-    puts input
 
     input.split('').each_with_index do |char, index|
       if has_lookahead?(input, index)
@@ -21,18 +20,14 @@ class RunLengthEncoding
   def self.decode(input)
     numbers_array = input.split(/\D/).select { |num| num.length > 0 }
     char_blocks = input.split(/\d/).select { |num| num.length > 0 }
-    input_remapped = []
     decoded = ""
     char_blocks.zip(numbers_array) do |left, right|
-      input_remapped += right.split('')
-      input_remapped << left if left
-    end
-
-    input_remapped.each_with_index do |item, index|
-      if(item.match(/^[\d]+$/))
-        decoded << (Array.new(item.to_i - 1) { input_remapped[index + 1] }).join('')
-      else
-        decoded << item
+      left.split('').each_with_index do |item, index|
+         if index == 0 && right
+           decoded << (Array.new(right.to_i) { item }).join('')
+         else
+           decoded << item
+         end
       end
     end
 
