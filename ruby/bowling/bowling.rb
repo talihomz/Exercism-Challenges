12b9555bf6da9f49c4077980d0051ff3f79e0235
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-03-13 08:19:30 +0100

    bowling more tests passed

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -1,62 +1,73 @@
 class Game
-    attr_reader :rolls
+  attr_reader :rolls
 
-    def initialize
-        @pins = []
-    end
+  class BowlingError < StandardError
+  end
 
-    def roll pins
-        @pins << pins
-    end
+  def initialize
+    @pins = []
+  end
 
-    def score
-       get_frame_score.inject(0) do |sum, item|
-        sum + item.inject(0) { |inner_sum, inner_item| inner_sum + inner_item }
-       end 
-    end
+  def roll pins
+    # validation
+    raise BowlingError.new if pins < 0 or pins > 10
+    raise BowlingError.new if (@pins[0] + pins) > 10
 
-    def get_frame_score
+    @pins << pins
+  end
 
-        all_frames = []
+  def score
+    get_frame_score.inject(0) do |sum, item|
+      sum + item.inject(0) { |inner_sum, inner_item| inner_sum + inner_item }
+    end 
+  end
+
+  def get_frame_score
+
+    all_frames = []
+    skipped = false
+
+    # build the frames
+    @pins.each_with_index do |item, index|
+      if skipped
         skipped = false
-
-        # build the frames
-        @pins.each_with_index do |item, index|
-            if skipped
-                skipped = false
-            elsif item == 10
-                all_frames << [10]
-            else
-                all_frames << @pins[index, 2]
-                skipped = true
-            end
-        end
-
-        result = []
-        # build bowling frame scorecard
-        begin  # change this dirty hack
-            all_frames.each_with_index do |frame, index|
-                if frame[0] == 10
-                    next_frame = Array.new(all_frames[index + 1])
-                    if next_frame[0] == 10
-                        next_frame << all_frames[index + 2][0]
-                    end
-                    result << frame + next_frame
-                elsif frame_sum(frame) == 10
-                    result << (frame << all_frames[index + 1][0])
-                else
-                    result << frame
-                end
-            end
-        rescue
-
-        end
-
-        # print result
-        result
+      elsif item == 10
+        all_frames << [10]
+      else
+        all_frames << @pins[index, 2]
+        skipped = true
+      end
     end
 
-    def frame_sum frame
-        frame.inject(0) do |sum, item| sum + item end 
+    result = []
+    # build bowling frame scorecard
+    begin
+      all_frames.each_with_index do |frame, index|
+        if frame[0] == 10
+          next if index >= all_frames.length - 1
+          # extract the next frame
+          next_frame = Array.new(all_frames[index + 1])
+
+          if next_frame[0] == 10
+            next_frame << all_frames[index + 2][0]
+          end
+
+          result << frame + next_frame
+        elsif frame_sum(frame) == 10
+          next if index >= all_frames.length - 1 
+          result << (Array.new(frame) << all_frames[index + 1][0])
+        else
+          next if index >= all_frames.length - 1 and frame_sum(all_frames[index - 1]) == 10
+          result << frame
+        end
+      end
+    rescue
     end
+
+    result
+  end
+
+  def frame_sum frame
+    frame.inject(0) do |sum, item| sum + item end 
+  end
 end
--- ruby/bowling/bowling_test.rb
+++ ruby/bowling/bowling_test.rb
@@ -11,133 +11,91 @@ class BowlingTest < Minitest::Test
     rolls.each { |pins| @game.roll(pins) }
   end
 
-<<<<<<< HEAD
   def test_should_be_able_to_score_a_game_with_all_zeros
-=======
-  def test_from_readme_should_work
-    record([10, 5, 5, 9, 0])
-    assert_equal 48, @game.score
-  end
-
-  def test_should_be_able_to_score_a_game_with_all_zeros
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
+    
     record([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 0, @game.score
   end
 
   def test_should_be_able_to_score_a_game_with_no_strikes_or_spares
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])
     assert_equal 90, @game.score
   end
 
   def test_a_spare_followed_by_zeros_is_worth_ten_points
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 10, @game.score
   end
 
   def test_points_scored_in_the_roll_after_a_spare_are_counted_twice
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 16, @game.score
   end
 
   def test_consecutive_spares_each_get_a_one_roll_bonus
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 31, @game.score
   end
 
   def test_a_spare_in_the_last_frame_gets_a_one_roll_bonus_that_is_counted_once
-    skip
+    
     record([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7])
     assert_equal 17, @game.score
   end
 
   def test_a_strike_earns_ten_points_in_a_frame_with_a_single_roll
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 10, @game.score
   end
 
   def test_points_scored_in_the_two_rolls_after_a_strike_are_counted_twice_as_a_bonus
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 26, @game.score
   end
 
   def test_consecutive_strikes_each_get_the_two_roll_bonus
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 81, @game.score
   end
 
   def test_a_strike_in_the_last_frame_gets_a_two_roll_bonus_that_is_counted_once
-    skip
+    
     record([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1])
     assert_equal 18, @game.score
   end
 
   def test_rolling_a_spare_with_the_two_roll_bonus_does_not_get_a_bonus_roll
-    skip
+    
     record([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3])
     assert_equal 20, @game.score
   end
 
   def test_strikes_with_the_two_roll_bonus_do_not_get_bonus_rolls
-    skip
+    
     record([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])
     assert_equal 30, @game.score
   end
 
   def test_a_strike_with_the_one_roll_bonus_after_a_spare_in_the_last_frame_does_not_get_a_bonus
-    skip
+    
     record([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10])
     assert_equal 20, @game.score
   end
 
   def test_all_strikes_is_a_perfect_game
-<<<<<<< HEAD
-    
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     record([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
     assert_equal 300, @game.score
   end
 
   def test_rolls_cannot_score_negative_points
-    skip
+    
     record([])
     assert_raises Game::BowlingError do
       @game.roll(-1)
@@ -145,7 +103,7 @@ class BowlingTest < Minitest::Test
   end
 
   def test_a_roll_cannot_score_more_than_10_points
-    skip
+    
     record([])
     assert_raises Game::BowlingError do
       @game.roll(11)
@@ -153,7 +111,6 @@ class BowlingTest < Minitest::Test
   end
 
   def test_two_rolls_in_a_frame_cannot_score_more_than_10_points
-    skip
     record([5])
     assert_raises Game::BowlingError do
       @game.roll(6)
@@ -256,24 +213,14 @@ class BowlingTest < Minitest::Test
   #
   # In your file, it will look like this:
   #
-<<<<<<< HEAD
   module BookKeeping
     VERSION = 3 # Where the version number matches the one in the test.
   end
-=======
-  # module BookKeeping
-  #   VERSION = 1 # Where the version number matches the one in the test.
-  # end
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
   #
   # If you are curious, read more about constants on RubyDoc:
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
   def test_bookkeeping
-<<<<<<< HEAD
     
-=======
-    skip
->>>>>>> 1bfabebfa73bcaf26c3be3b4bcaae940ae338637
     assert_equal 3, BookKeeping::VERSION
   end
 end
