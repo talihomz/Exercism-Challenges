2e718e7ca03d538ea9b282f9e54bf57f4d6b84f1
Kevin Wahome <wahomekevin@gmail.com>
2018-03-14 09:59:12 +0300

    Started Bowling refactor

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -1,70 +1,86 @@
 class Game
-  attr_reader :rolls
-
   class BowlingError < StandardError
   end
 
   def initialize
-    @pins = []
+    @frames = {}
+    @current_frame = []
   end
 
   def roll pins
-    # validation
-    raise BowlingError.new if pins < 0 or pins > 10
-    raise BowlingError.new if (@pins[0] + pins) > 10
+    # input validation
+    raise BowlingError if pins < 0 or pins > 10
 
-    @pins << pins
+    # frame validation
+    raise BowlingError unless @frames[10].nil?
+
+    @current_frame << pins
+
+    process_frames
+  end
+
+  def process_frames
+    # add frame scoring logic
+    # include strike logic
+    # include spare logic
+
+    if(is_full?)
+      @frames[@frames.length + 1] = Array.new(@current_frame)
+      @current_frame.clear
+    end
+  end
+
+  def is_full?
+    raise BowlingError if frame_sum(@current_frame) > 10
+    @current_frame.include?(10) || @current_frame.length == 2
   end
 
   def score
-    get_frame_score.inject(0) do |sum, item|
-      sum + item.inject(0) { |inner_sum, inner_item| inner_sum + inner_item }
-    end 
+    raise BowlingError if @frames.empty? || @frames[10].nil?
   end
 
   def get_frame_score
 
-    all_frames = []
-    skipped = false
+  #   all_frames = []
+  #   skipped = false
 
-    # build the frames
-    @pins.each_with_index do |item, index|
-      if skipped
-        skipped = false
-      elsif item == 10
-        all_frames << [10]
-      else
-        all_frames << @pins[index, 2]
-        skipped = true
-      end
-    end
+  #   # build the frames
+  #   @pins.each_with_index do |item, index|
+  #     if skipped
+  #       skipped = false
+  #     elsif item == 10
+  #       all_frames << [10]
+  #     else
+  #       all_frames << @pins[index, 2]
+  #       skipped = true
+  #     end
+  #   end
 
-    result = []
-    # build bowling frame scorecard
-    begin
-      all_frames.each_with_index do |frame, index|
-        if frame[0] == 10
-          next if index >= all_frames.length - 1
-          # extract the next frame
-          next_frame = Array.new(all_frames[index + 1])
+  #   result = []
+  #   max_frame_index = all_frames.length - 1
+  #   # build bowling frame scorecard
+  #   all_frames.each_with_index do |frame, index|
+  #     if frame[0] == 10
+  #       next if index >= max_frame_index
+  #       # extract the next frame
+  #       next_frame = Array.new(all_frames[index + 1])
 
-          if next_frame[0] == 10
-            next_frame << all_frames[index + 2][0]
-          end
+  #       if next_frame[0] == 10
+  #         next if (index + 2) > max_frame_index
+  #         next_frame << all_frames[index + 2][0]
+  #       end
 
-          result << frame + next_frame
-        elsif frame_sum(frame) == 10
-          next if index >= all_frames.length - 1 
-          result << (Array.new(frame) << all_frames[index + 1][0])
-        else
-          next if index >= all_frames.length - 1 and frame_sum(all_frames[index - 1]) == 10
-          result << frame
-        end
-      end
-    rescue
-    end
+  #       result << frame + next_frame
+  #     elsif frame_sum(frame) == 10
+  #       next if (index + 1) > max_frame_index
+  #       result << (Array.new(frame) << all_frames[index + 1][0])
+  #     else
+  #       next if index >= max_frame_index and frame_sum(all_frames[index - 1]) == 10
+  #       result << frame
+  #     end
+  #   end   
 
-    result
+  #   result
   end
 
   def frame_sum frame
